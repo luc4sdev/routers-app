b23709608a7060b3f63f77b8504b43d8af65e878
Lucas Pereira França <lucas.pfranca01@gmail.com>
2024-05-09 16:37:38 -0300

    feat: add middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/',
-            destination: '/clients',
-            permanent: false,
-          },
-        ]
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Sidebar } from "./components/Sidebar";
 import { ThemeProvider } from "next-themes";
