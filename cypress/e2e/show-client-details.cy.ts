6e6d85f0b0fda6f6c7e79bbdf7770d5fd38192d5
Lucas Pereira França <lucas.pfranca01@gmail.com>
2024-05-09 23:20:41 -0300

    fix: remove tests with api

--- cypress/e2e/show-client-details.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('show client details', () => {
-    it('should be able to click in client details button and show client details', () => {
-        cy.visit('http://localhost:3000/clients')
-        cy.contains('Ver detalhes do cliente').click();
-
-        cy.get('[role="dialog"]').should('be.visible');
-    })
-})
--- cypress/e2e/show-router-details.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('show router details', () => {
-    it('should be able to click in router details button and show router details', () => {
-        cy.visit('http://localhost:3000/routers')
-        cy.contains('Ver detalhes do roteador').click();
-
-        cy.get('[role="dialog"]').should('be.visible');
-    })
-})
